f722bb79ab7fb467d9dc3778273cac4785a97dc0
zacsinger <55707744+zacharysinger@users.noreply.github.com>
2020-07-01 17:09:07 -0700

    mobile an desktop changes

--- static/scripts/scroll.js
+++ static/scripts/scroll.js
@@ -4,6 +4,7 @@ $("a").click(function(){
   var pageId3 = $(this).attr("data-page3");
   var pageId4 = $(this).attr("data-page4");
   var pageId5 = $(this).attr("data-page5");
+  var pageId6 = $(this).attr("data-page6");
 
   if ( $( this ).hasClass( "1" ) ) {
     $("html, body").animate({scrollTop: $("#" + pageId1).offset().top - 100}, 1000);
@@ -33,6 +34,12 @@ $("a").click(function(){
     $("html, body").animate({scrollTop: $("#" + pageId5).offset().top - 55}, 1000);
     $("#mainListDiv").toggleClass("show_list");
     $(".navTrigger").toggleClass('active');
+  }
+
+  if ( $( this ).hasClass( "6" ) ) {
+    $("html, body").animate({scrollTop: $("#" + pageId6).offset().top - 40}, 1000);
+    $("#mainListDiv").removeClass("show_list");
+    $(".navTrigger").removeClass('active');
   }
 
 });
